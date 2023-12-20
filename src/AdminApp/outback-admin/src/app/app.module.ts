6a30cd48f66649b412483b504058718718baba34
Rinsen <fredrikh@outlook.com>
2023-12-20 19:58:43 +0100

    Updated typescript

--- src/AdminApp/outback-admin/src/app/app.module.ts
+++ src/AdminApp/outback-admin/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatRadioModule } from '@angular/material/radio';
-import { ClientClient, ScopeClient } from './modules/api/api.generated';
+import { ClientClient, ScopeClient } from './services/generated/api.generated';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
