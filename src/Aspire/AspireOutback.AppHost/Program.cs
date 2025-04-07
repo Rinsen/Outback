47db153d08b85d7d884c085c156112dc8d954849
Fredrik Rinsén <Rinsen@users.noreply.github.com>
2025-04-08 00:06:13 +0200

    A bit closer to a working state (#3)

--- src/Aspire/AspireOutback.AppHost/Program.cs
+++ src/Aspire/AspireOutback.AppHost/Program.cs
@@ -2,18 +2,25 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sqlServer = builder.AddSqlServer("outback")
+var sqlServer = builder.AddSqlServer("outbackserver")
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
-builder.AddProject<Projects.Rinsen_Outback_App_Installation>("outbackinstallation")
+var db = sqlServer.AddDatabase("Outback");
+
+var installation = builder.AddProject<Projects.Rinsen_Outback_App_Installation>("outbackinstallation")
     .WithReference(sqlServer)
     .WaitFor(sqlServer)
     .WithEnvironment("Command", "Install")
     .WithEnvironment("DatabaseName", "Outback")
     .WithEnvironment("Schema", "dbo")
     .WithEnvironment("Login__Debug__Password", "nfgsjknFSDgdf5436545fghscnhfgmDFSAFdfsj4534DFG")
-    .WithEnvironment("Login__Runtime__Password", "fds4235sfdgDFGfgde4523sdfgSDdcsfsfdg32")
+    .WithEnvironment("Login__Runtime__Password", "fds4235sfdgDFGfgde4523sdfgSDdcsfsfdg32");
+
+builder.AddProject<Projects.Rinsen_Outback_App>("outbackapp")
+    .WithReference(db)
+    .WaitFor(db)
+    .WaitFor(installation)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
