e6dd91d429390883cedfbca8456b603ca7592d72
Rinsen <fredrikh@outlook.com>
2025-04-07 23:34:24 +0200

    Working aspire integration for installation

--- sample/SampleServer/Startup.cs
+++ sample/SampleServer/Startup.cs
@@ -47,6 +47,10 @@ namespace SampleServer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -61,7 +65,7 @@ namespace SampleServer
             {
                 routes.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Identity}/{action=Index}");
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
---
+++ src/Aspire/AspireOutback.AppHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var sqlServer = builder.AddSqlServer("outback")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+builder.AddProject<Projects.Rinsen_Outback_App_Installation>("outbackinstallation")
+    .WithReference(sqlServer)
+    .WaitFor(sqlServer)
+    .WithEnvironment("Command", "Install")
+    .WithEnvironment("DatabaseName", "Outback")
+    .WithEnvironment("Schema", "dbo")
+    .WithEnvironment("Login__Debug__Password", "nfgsjknFSDgdf5436545fghscnhfgmDFSAFdfsj4534DFG")
+    .WithEnvironment("Login__Runtime__Password", "fds4235sfdgDFGfgde4523sdfgSDdcsfsfdg32")
+    .WithExternalHttpEndpoints();
+
+builder.Build().Run();
